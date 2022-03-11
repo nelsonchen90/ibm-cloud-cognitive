2314fb4a823f9b1d0410e7131f34be94afc2e3f7
Holman Sze <hoiwingh@ca.ibm.com>
2022-03-11 16:11:18 -0500

    feat(Datagrid): add initial load story

--- packages/cloud-cognitive/src/components/Datagrid/Datagrid.stories.js
+++ packages/cloud-cognitive/src/components/Datagrid/Datagrid.stories.js
@@ -5,47 +5,47 @@
  * LICENSE file in the root directory of this source tree.
  */
 
- import React, { useState } from 'react';
- // TODO: import action to handle events if required.
- // import { action } from '@storybook/addon-actions';
- import namor from 'namor';
- 
- import {
-   getStoryTitle,
-   prepareStory,
- } from '../../global/js/utils/story-helper';
- 
- import { Datagrid, useDatagrid } from '.';
- import mdx from './Datagrid.mdx';
- 
- import styles from './_storybook-styles.scss';
- 
- export default {
-   title: getStoryTitle(Datagrid.displayName),
-   component: Datagrid,
-   // TODO: Define argTypes for props not represented by standard JS types.
-   // argTypes: {
-   //   egProp: { control: 'color' },
-   // },
-   parameters: {
-     styles,
-     docs: {
-       page: mdx,
-     },
-   },
- };
- 
- const range = (len) => {
-   const arr = [];
-   for (let i = 0; i < len; i++) {
-     arr.push(i);
-   }
-   return arr;
- };
- 
- const newPerson = () => {
-   const statusChance = Math.random();
-   return {
+import React, { useState, useEffect } from 'react';
+// TODO: import action to handle events if required.
+// import { action } from '@storybook/addon-actions';
+import namor from 'namor';
+
+import {
+  getStoryTitle,
+  prepareStory,
+} from '../../global/js/utils/story-helper';
+
+import { Datagrid, useDatagrid } from '.';
+import mdx from './Datagrid.mdx';
+
+import styles from './_storybook-styles.scss';
+
+export default {
+  title: getStoryTitle(Datagrid.displayName),
+  component: Datagrid,
+  // TODO: Define argTypes for props not represented by standard JS types.
+  // argTypes: {
+  //   egProp: { control: 'color' },
+  // },
+  parameters: {
+    styles,
+    docs: {
+      page: mdx,
+    },
+  },
+};
+
+const range = (len) => {
+  const arr = [];
+  for (let i = 0; i < len; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+const newPerson = () => {
+  const statusChance = Math.random();
+  return {
     firstName: namor.generate({ words: 1, numbers: 0 }),
     lastName: namor.generate({ words: 1, numbers: 0 }),
     age: Math.floor(Math.random() * 30),
@@ -71,101 +71,101 @@
     someone18: namor.generate({ words: 1, numbers: 0 }),
     someone19: namor.generate({ words: 1, numbers: 0 }),
     someone20: namor.generate({ words: 1, numbers: 0 }),
-     status:
-       statusChance > 0.66
-         ? 'relationship'
-         : statusChance > 0.33
-           ? 'complicated'
-           : 'single',
-   };
- };
- 
- const makeData = (...lens) => {
-   const makeDataLevel = (depth = 0) => {
-     const len = lens[depth];
-     return range(len).map(() => ({
-       ...newPerson(),
-       subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
-     }));
-   };
- 
-   return makeDataLevel();
- }
- 
- const defaultHeader = [
-   {
-     Header: 'Row Index',
-     accessor: (row, i) => i,
-     sticky: "left",
-     id: 'rowIndex', // id is required when accessor is a function.
-   },
-   {
-     Header: 'First Name',
-     accessor: 'firstName',
-     sticky: "left",
-   },
-   {
-     Header: 'Last Name',
-     accessor: 'lastName',
-   },
-   {
-     Header: 'Age',
-     accessor: 'age',
-     width: 50,
-   },
-   {
-     Header: 'Visits',
-     accessor: 'visits',
-     width: 60,
-   },
-   {
-     Header: 'Someone 1',
-     accessor: 'someone1',
-   },
-   {
-     Header: 'Someone 2',
-     accessor: 'someone2',
-   },
-   {
-     Header: 'Someone 3',
-     accessor: 'someone3',
-   },
-   {
-     Header: 'Someone 4',
-     accessor: 'someone4',
-   },
-   {
-     Header: 'Someone 5',
-     accessor: 'someone5',
-   },
-   {
-     Header: 'Someone 6',
-     accessor: 'someone6',
-   },
-   {
-     Header: 'Someone 7',
-     accessor: 'someone7',
-   },
-   {
-     Header: 'Someone 8',
-     accessor: 'someone8',
-   },
-   {
-     Header: 'Someone 9',
-     accessor: 'someone9',
-   },
-   {
-     Header: 'Someone 10',
-     accessor: 'someone10',
-   },
- ];
- 
- /**
-  * TODO: Declare template(s) for one or more scenarios.
-  */
+      status:
+        statusChance > 0.66
+          ? 'relationship'
+          : statusChance > 0.33
+            ? 'complicated'
+            : 'single',
+  };
+};
+
+const makeData = (...lens) => {
+  const makeDataLevel = (depth = 0) => {
+    const len = lens[depth];
+    return range(len).map(() => ({
+      ...newPerson(),
+      subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined,
+    }));
+  };
+
+  return makeDataLevel();
+}
+
+const defaultHeader = [
+  {
+    Header: 'Row Index',
+    accessor: (row, i) => i,
+    sticky: "left",
+    id: 'rowIndex', // id is required when accessor is a function.
+  },
+  {
+    Header: 'First Name',
+    accessor: 'firstName',
+    sticky: "left",
+  },
+  {
+    Header: 'Last Name',
+    accessor: 'lastName',
+  },
+  {
+    Header: 'Age',
+    accessor: 'age',
+    width: 50,
+  },
+  {
+    Header: 'Visits',
+    accessor: 'visits',
+    width: 60,
+  },
+  {
+    Header: 'Someone 1',
+    accessor: 'someone1',
+  },
+  {
+    Header: 'Someone 2',
+    accessor: 'someone2',
+  },
+  {
+    Header: 'Someone 3',
+    accessor: 'someone3',
+  },
+  {
+    Header: 'Someone 4',
+    accessor: 'someone4',
+  },
+  {
+    Header: 'Someone 5',
+    accessor: 'someone5',
+  },
+  {
+    Header: 'Someone 6',
+    accessor: 'someone6',
+  },
+  {
+    Header: 'Someone 7',
+    accessor: 'someone7',
+  },
+  {
+    Header: 'Someone 8',
+    accessor: 'someone8',
+  },
+  {
+    Header: 'Someone 9',
+    accessor: 'someone9',
+  },
+  {
+    Header: 'Someone 10',
+    accessor: 'someone10',
+  },
+];
+
+/**
+* TODO: Declare template(s) for one or more scenarios.
+*/
 
 
- const BasicUsage = () => {
+const BasicUsage = () => {
   const columns = React.useMemo(() => defaultHeader, []);
   const [data] = useState(makeData(10));
   const datagridState = useDatagrid(
@@ -176,23 +176,40 @@
   );
 
   return <Datagrid {...datagridState} />;
- };
- /**
-  * TODO: Declare one or more stories, generally one per design scenario.
-  * NB no need for a 'Playground' because all stories have all controls anyway.
-  */
- export const dataGridBasicUsage = prepareStory(BasicUsage, {
-   args: {},
- });
- 
+};
 
-/**
- * TODO: Declare one or more stories, generally one per design scenario.
- * NB no need for a 'Playground' because all stories have all controls anyway.
- */
-// export const dataGrid = prepareStory(template, {
-//   args: {
-//     // TODO: Component args - https://storybook.js.org/docs/react/writing-stories/args#Datagrid-args
-//     children: 'hello, world',
-//   },
-// });
+const InitialLoad = () => {
+  const columns = React.useMemo(() => defaultHeader, []);
+  const [data, setData] = useState(makeData(0));
+
+  const [isFetching, setIsFetching] = useState(false);
+  const fetchData = () => new Promise((resolve) => {
+    setIsFetching(true);
+    setTimeout(() => {
+      setData(data.concat(makeData(30, 5, 2)));
+      resolve();
+    }, 1000);
+  }).then(() => setIsFetching(false) );
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const datagridState = useDatagrid(
+    {
+      columns,
+      data,
+      isFetching,
+    },
+  );
+
+  return <Datagrid {...datagridState} />;
+};
+
+export const dataGridBasicUsage = prepareStory(BasicUsage, {
+  args: {},
+});
+ 
+export const dataGridInitialLoad = prepareStory(InitialLoad, {
+  args: {},
+});
